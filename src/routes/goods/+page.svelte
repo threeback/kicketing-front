a406560719c5380bbb10df1225b09a2d23186046
cottonrose1011 <pjeka7er43@gmail.com>
2024-06-01 23:56:25 +0900

    feat : 공연 상세페이지 기능 API
    - 잔여좌석, 예매 가능한 날짜를 포함한 공연의 정보를 보여줍니다.

--- src/routes/goods/+page.svelte
+++ src/routes/goods/+page.svelte
@@ -1,66 +1,47 @@
-<script>
-    import dayjs from 'dayjs';
-    import 'dayjs/locale/ko.js';
-    import { Datepicker } from 'svelte-calendar';
-
-    dayjs.locale('ko');
-
-    const theme = {
-        calendar: {
-            width: '500px',
-            maxWidth: '100vw',
-            legend: {
-                height: '45px'
-            },
-            shadow: '0px 0px 5px rgba(0, 0, 0, 0.25)',
-            colors: {
-                background: {
-                    highlight: '#007bff'
-                }
-            },
-            font: {
-                regular: '0.9em',
-                larger: '37em'
-            }
-        }
-    };
-
-    let store;
-
-    let title = '현대 뮤지컬 공연';
-    let ganre = '뮤지컬';
-    let posterUrl = 'https://kicketing.s3.ap-northeast-2.amazonaws.com/poster/%E1%84%86%E1%85%B2%E1%84%8C%E1%85%B5%E1%84%8F%E1%85%A5%E1%86%AF.jpg';
-    let place = {
-        id: 1,
-        name: 'LG아트센터',
-        address: '서울특별시 강서구 마곡중앙로 136',
-        hall: 'SIGNATURE홀'
-    };
-    let length = 90;
-    let today = new Date();
-    let startDate = new Date('2024.07.01');
-    let endDate = new Date('2024.07.05');
-    let ageLimit = 12;
-    let prices = [
-        { type: 'VIP', price: 160000 },
-        { type: 'R', price: 80000 },
-        { type: 'S', price: 60000 },
-        { type: 'A', price: 35000 }
-    ];
-    let stars = [
-        { id: 1, name: '이현석', birthdate: '1999.01.01', gender: 'MALE', imageURL: 'http://example.com/images/msic.jpg' },
-        { id: 3, name: '정부용', birthdate: '2001.03.03', gender: 'MALE', imageURL: 'http://example.com/images/msic.jpg' },
-        { id: 4, name: '전지환', birthdate: '2002.04.05', gender: 'MALE', imageURL: 'http://example.com/images/msic.jpg' },
-        { id: 6, name: '카리나', birthdate: '1992.03.06', gender: 'FEMALE', imageURL: 'http://example.com/images/msic.jpg' },
-    ]
-    let gender = '';
-
-    async function handleBookingListener() {
-
-    }
-</script>
-
 <style>
+    :global(.datepicker[data-picker-theme="custom-datepicker"]) {
+        --datepicker-container-background: #8A2BE2;
+        --datepicker-container-border: 1px solid #8A2BE2;
+
+        --datepicker-calendar-header-text-color: #fff;
+        --datepicker-calendar-dow-color: #fff;
+        --datepicker-calendar-day-color: #fff;
+        --datepicker-calendar-day-color-disabled: #8A2BE2;
+        --datepicker-calendar-range-selected-background: #8A2BE2;
+
+        --datepicker-calendar-header-month-nav-background-hover: #BA55D3;
+        --datepicker-calendar-header-month-nav-icon-next-filter: invert(100);
+        --datepicker-calendar-header-month-nav-icon-prev-filter: invert(100);
+        --datepicker-calendar-header-year-nav-icon-next-filter: invert(100);
+        --datepicker-calendar-header-year-nav-icon-prev-filter: invert(100);
+
+        --datepicker-calendar-split-border: 1px solid orchid;
+
+        --datepicker-presets-border: 1px solid orchid;
+        --datepicker-presets-button-background-active: #8A2BE2;
+        --datepicker-presets-button-color: #fff;
+        --datepicker-presets-button-color-active: #fff;
+        --datepicker-presets-button-color-hover: #333;
+        --datepicker-presets-button-color-focus: #333;
+    }
+
+    .submit-button {
+        background-color: #007bff; /* 로그인 버튼 파란색으로 변경 */
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+        border-radius: 5px;
+        transition: background-color 0.3s, transform 0.3s; /* 애니메이션 효과 추가 */
+    }
+
+    .submit-button:hover {
+        background-color: #0056b3; /* 호버링 시 더 진한 파란색 */
+        transform: scale(1.05); /* 호버링 시 약간 커짐 */
+    }
+
+
     .container {
         margin: 20px;
         max-width: 1200px;
@@ -173,54 +154,234 @@
     }
 </style>
 
-<title>{title}</title>
-<main class="container">
-    <h1>공연 상세 정보</h1>
+<script>
+    import dayjs from 'dayjs';
+    import 'dayjs/locale/ko.js';
+    import {onMount} from 'svelte';
+    import {endpoints} from "$lib/api.js";
+    import {DatePicker} from "@svelte-plugins/datepicker";
+    import {format} from 'date-fns';
+    import {goto} from "$app/navigation";
+
+    dayjs.locale('ko');
+
+    let performanceUUID = "";
+    let queryParams = {};
+    let performanceDTO = [];
+    let placeDTO = [];
+    let seatGrades = [];
+    let stars = [];
+    let bookableDates = [];
+    let specificDateObject = [];
+    let enabledDates = [];
+    let myDates = [];
+    let seatLefts = [];
+    let startDate = new Date().toDateString()
+    let endDate;
+    let dateFormat = 'yyyy년 MM월 dd일';
+    let fetchFormat = 'yyyyMMdd';
+    let enableFormat = 'yyyy.MM.dd'
+    let bookableFormat = 'yyyy-MM-dd'
+    let isOpen = false;
+    let enableFutureDates = true;
+    let enablePastDates = false;
+    let selected;
+
+
+    const toggleDatePicker = () => (isOpen = !isOpen);
+
+    const formatDate = (dateString) => {
+        let myDate = dateString && format(new Date(dateString), fetchFormat) || '';
+        if (myDate !== (new Date() && format(new Date(), fetchFormat) || '')) {
+            getBookableDates(myDate)
+        }
+        return dateString && format(new Date(dateString), dateFormat) || '';
+    };
+
+    const bookableFormatDate = (dateString) => {
+        return dateString && format(new Date(dateString), bookableFormat) || '';
+    };
+
+    let formattedStartDate = formatDate(startDate);
+
+    $: formattedStartDate = formatDate(startDate)
+
+
+    onMount(async () => {
+        const params = new URLSearchParams(window.location.search);
+        params.forEach((value, key) => {
+            queryParams[key] = value;
+            performanceUUID = value
+        });
+        try {
+            const response = await fetch(endpoints.performance + "/" + performanceUUID, {
+                method: "GET",
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            performanceDTO = (result.performanceDTO);
+            placeDTO = (result.placeDTO);
+            seatGrades = (result.seatGrades);
+            stars = (result.stars);
+            await getBookableDates(new Date() && format(new Date(), fetchFormat) || '');
+        } catch (err) {
+            console.log(err.message);
+        }
+    });
+
+    async function getBookableDates(selected) {
+        try {
+            const response = await fetch(endpoints.performance + "/" + performanceDTO.id + "/bookable-dates?startDate=" + selected, {
+                method: "GET",
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            bookableDates = result.bookableDates;
+
+            bookableDates.forEach(
+                bookableDate => {
+                    let daysOfMonth = new Date(bookableDate.dateTime) && format(new Date(bookableDate.dateTime), enableFormat) || '';
+                    myDates.push(new Date(daysOfMonth).toDateString());
+                    enabledDates = Array.from(new Set(myDates));
+                }
+            )
+
+            specificDateObject = Array.from(bookableDates.filter(item => item.dateTime.split('T')[0] === bookableFormatDate(startDate)));
+            specificDateObject.sort((a, b) => a.round - b.round);
+            // console.log(specificDateObject)
+        } catch (err) {
+            console.log(err.message);
+        }
+
+    }
+
+    function handleNavigationChange(event) {
+        if (event.direction === 'next' && (event.currentPeriod.start <= performanceDTO.endDate)) {
+            getBookableDates(
+                new Date(event.currentPeriod.start)
+                && format(new Date(event.currentPeriod.start), fetchFormat) || '')
+        }
+    }
+
+    function handleDayChange(event) {
+
+        if (formatDate(event.startDate) === formatDate(new Date())) {
+            getBookableDates(event.startDate && format(new Date(event.startDate), fetchFormat) || '')
+        }
+    }
+
+    async function handleBookingListener(specific) {
+        goto(`/booking?performanceUUID=${performanceUUID}&onStageId=${specific.id}`)
+    }
+
+    async function handleChangeSeats(specific) {
+        try {
+            const response = await fetch(endpoints.performance + "/" + specific.id + "/bookable-seats", {
+                method: "GET",
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            seatLefts = result.seatGradeCounts;
+            let counts = 0;
+            seatLefts.forEach((seatLeft) => {
+                counts += seatLeft.count
+            })
+            const button = document.getElementById('submit-button')
+            if (counts < 1) {
+                button.disabled = true;
+            } else {
+                button.disabled = false;
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+</script>
+
+<title>{performanceDTO.name}</title>
+<main class="container" style="align-self: center;">
+    <div>
+        <h2 class="title">{performanceDTO.name}</h2>
+        <p style="margin-top: -10px; margin-left: 5px; margin-bottom: 20px">{performanceDTO.genre}</p>
+    </div>
     <hr>
-    <h2 class="title">{title}</h2>
-    <p style="margin-top: -10px; margin-left: 5px; margin-bottom: 20px">{ganre}</p>
-    <div class="content">
+    <div class="content" style="margin-left: 10px; padding: 30px">
         <div class="poster-container">
-            <img class="poster" src={posterUrl} alt="Poster">
+            <img class="poster" src={performanceDTO.imageUrl} alt="Poster">
         </div>
         <div class="info">
             <div class="info-item">
-                <span class="info-label">장소: {place.name}</span>
-                <span style="font-size: 15px">({place.hall})</span>
+                <span class="info-label">장소: {placeDTO.name}</span>
+                <span class="info-label">{placeDTO.hall}</span>
             </div>
-            <p style="margin-top: -5px; margin-left: 55px; color: gray">{place.address}</p>
+            <p style="margin-top: -5px; margin-left: 55px; color: gray">{placeDTO.address}</p>
             <div class="info-item">
-                <span class="info-label">공연기간: {dayjs(startDate).format('YYYY년 M월 D일')} ~ {dayjs(endDate).format('YYYY년 M월 D일')}</span>
-            </div>
-            <div class="info-item">
-                <span class="info-label">공연시간: {length}분</span>
+                <span class="info-label">공연기간: {dayjs(new Date(performanceDTO.startDate)).format('YYYY년 M월 D일')}
+                    ~ {dayjs(new Date(performanceDTO.endDate)).format('YYYY년 M월 D일')}</span>
             </div>
             <div class="info-item">
-                <span class="info-label">관람연령: {ageLimit}세 이상 관람가</span>
+                <span class="info-label">공연시간: {performanceDTO.length}분</span>
+            </div>
+            <div class="info-item">
+                <span class="info-label">관람 연령: {performanceDTO.ageLimit}세 이상</span>
             </div>
             <div class="price-section">
                 <div class="price-title">
                     <span>좌석 등급별 가격</span>
                 </div>
                 <div class="price-list">
-                    {#each prices as price}
+                    {#each seatGrades as seat}
                         <div class="price-item">
-                            <span class="price-type">{price.type}</span>
-                            <span class="price-value">{price.price.toLocaleString()}원</span>
+                            <span class="price-type">{seat.grade}&nbsp;&nbsp;&nbsp;</span>
+                            <span class="price-value">{seat.price.toLocaleString()}&nbsp;원 </span>
                         </div>
                     {/each}
                 </div>
-                <Datepicker bind:store let:key let:send let:receive start={ startDate > today ? startDate : today } end={endDate} {theme}>
-                    <button class="blue-button" in:receive|local={{ key }} out:send|local={{ key }}>
-                        {#if $store?.hasChosen}
-                            {dayjs($store.selected).format('YYYY년 M월 D일 ddd요일')}
-                        {:else}
-                            관람일 선택하기
-                        {/if}
-                    </button>
-                </Datepicker>
             </div>
-<!--            <button class="blue-button" on:click={handleBookingListener} style="width: 165px">예매하러 가기</button>-->
+        </div>
+        <div>
+            <div>공연 날짜 선택하기</div>
+            <br>
+            <DatePicker bind:isOpen bind:startDate bind:endDate bind:enabledDates bind:enableFutureDates
+                        bind:enablePastDates onNavigationChange={handleNavigationChange} onDayClick={handleDayChange}>
+            </DatePicker>
+            <input type="text" placeholder="Select date" bind:value={formattedStartDate}
+                   on:click={toggleDatePicker} style="margin-bottom: 20px;"/>
+            <div style="margin-bottom: 60px;">
+                <div style="margin-bottom: 5px;">회차/좌석 정보</div>
+                <hr>
+                <select bind:value={selected} on:change={() => handleChangeSeats(selected)}
+                        style="width: 165px; height: 50px; margin-bottom: 20px; font-size: 16px; border: 1px solid #ccc; border-radius: 5px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
+                    {#each specificDateObject as specific}
+                        <option value={specific}
+                                style="padding: 10px 0;">
+                            &nbsp;&nbsp;&nbsp;{specific.round}회차 - {specific.dateTime.split('T')[1].slice(0, 5)}
+                        </option>
+                    {/each}
+                </select>
+                <br>
+                <div style="margin-bottom: 5px">
+                    {#if seatLefts.length > 0}
+                        {#each seatLefts as seat}
+                            {seat.grade}등급 :&nbsp;{seat.count}&nbsp;석
+                        {/each}
+                    {/if}
+                </div>
+            </div>
+            <div>
+                <br>
+                <button id="submit-button" class="submit-button" style="width: 165px"
+                        on:click={()=> handleBookingListener(selected)}>
+                    예매하기
+                </button>
+            </div>
         </div>
     </div>
     <hr>
@@ -228,10 +389,10 @@
     <div class="cast-list">
         {#each stars as star}
             <div class="cast-member">
-                <img src='src/lib/images/main/iu_poster.jpg' alt="">
-<!--                <img src={star.imageURL} alt="">-->
+                <img src={star.imageURL} alt="">
                 <div class="details">
-                    <span style="font-weight: bold">{star.name}</span><span style="font-size: 15px; margin-left: 3px">{gender} { star.gender === 'MALE' ? '남' : '여' }</span>
+                    <span style="font-weight: bold">{star.name}</span><span
+                        style="font-size: 15px; margin-left: 3px"> { star.sex === 'MALE' ? '(남)' : '(여)' }</span>
                     <p>{star.birthdate}</p>
                 </div>
             </div>
